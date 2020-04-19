883876597815239ba7455991073a28d35bd7932a
Daniel Zen <github@zendigital.com>
2020-04-18 22:22:27 -0400

    data driven

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
 
+interface Item {
+  title: string;
+  done: boolean;
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public items: Item [];
 
-  constructor() {}
+  constructor() {
+    this.items = [
+      {title: 'ion-item', done: true},
+      {title: 'Data Drive the App', done: true},
+      {title: 'Toggle Items', done: false},
+      {title: 'Create New Items', done: false},
+      {title: 'Delete Items', done: false},
+      {title: 'Edit Items', done: false},
+    ];
+  }
 
 }
